ab40c834817ce23b7a81c5614d5f70eeb66ea03a
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-10 10:35:07 +0100

    Configure store with combineReducers and persist data in store

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import {
+  BrowserRouter as Router, Route, Switch,
+} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Signup from './Signup';
+import Login from './Login';
+// import NavBar from '../components/NavBar';
+// import Home from '../components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
+
+    <div className="container">
+      <div className="row">
+        <Router>
+          <div className="col-3">
+            {/* <NavBar /> */}
+          </div>
+          <Switch>
+            <div className="col-9">
+              <Route exact path="/Signup" component={Signup} />
+              <Route exact path="/Login" component={Login} />
+              <Route exact path="/" component={Home} />
+            </div>
+          </Switch>
+        </Router>
+      </div>
+
     </div>
+
   );
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,37 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './userSlice';
+import typeReducer from './typeSlice';
+
+const reducers = combineReducers({
+  user: userReducer,
+  type: typeReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 const store = configureStore({
-  reducer: {
-
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 
 export default store;
