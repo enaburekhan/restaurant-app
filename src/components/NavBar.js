7a5498c6caf183068c7eee7cb9d7e481401ccdf9
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-13 07:18:56 +0100

    Fix styling for NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,23 +10,22 @@ const NavBar = () => {
   return (
 
     <nav className="nav">
-      <p>Restaurants Collections</p>
+      <h5>Restaurants Collections</h5>
 
       <div className="nav-home d-flex flex-column justify-content-between align-items-center  m-4">
         <div>
-          {user && user.username}
+          <p className="username">{user && user.username}</p>
           {token ? (
             <div>
               <Link to="/restaurants" className="doc nav-link active">Restaurants</Link>
-              <Link to="/collections" className="appoint-md nav-link">Collections</Link>
-              <Link to="/collections/new" className="appoint-md nav-link">NewCollections</Link>
+              <Link to="/collections" className="doc appoint-md nav-link">Collections</Link>
+              <Link to="/collections/new" className="doc appoint-md nav-link">NewCollections</Link>
               <Logout />
             </div>
           ) : (
             <div className="nav-home">
-              <Link to="/" className="nav-link">Home</Link>
-              <Link to="/Signup" className="nav-link">Signup</Link>
-              <Link to="/Login" className="nav-link">Login</Link>
+              <Link to="/Signup" className="doc nav-link">Signup</Link>
+              <Link to="/Login" className="doc nav-link">Login</Link>
             </div>
           )}
         </div>
